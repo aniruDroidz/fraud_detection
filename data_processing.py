8577c21d269498c450bf2e43ca8639babfd4cc62
Aniruddha Trikoliker <aniruddha.trikoliker@gmail.com>
2022-10-29 16:19:03 +0530

    added a new function sum

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,13 @@
-print("Adding all the data processing code related to fraud detection")
+from re import A
+
+
+print("Adding all the data processing code related to fraud detection ")
+
+a = 5
+b = 7
+
+def sum(a,b):
+    c = a+b
+    return A
+
+print("added a new function sum which is not correct")
