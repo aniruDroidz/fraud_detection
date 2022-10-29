e8b2ddef76025d52095164321b3c39941ecdc0f0
Aniruddha Trikoliker <aniruddha.trikoliker@gmail.com>
2022-10-29 16:19:48 +0530

    Revert "added a new function sum"
    This reverts commit 8577c21d269498c450bf2e43ca8639babfd4cc62.

--- data_processing.py
+++ data_processing.py
@@ -1,13 +1,1 @@
-from re import A
-
-
-print("Adding all the data processing code related to fraud detection ")
-
-a = 5
-b = 7
-
-def sum(a,b):
-    c = a+b
-    return A
-
-print("added a new function sum which is not correct")
+print("Adding all the data processing code related to fraud detection")
